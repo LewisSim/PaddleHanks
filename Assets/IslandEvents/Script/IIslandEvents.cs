ffe64dd1fae455b9ad3df2f19af6163b42b3ad6c
LewisSim <57391605+LewisSim@users.noreply.github.com>
2021-03-17 17:30:12 +0000

    Island Events comments

--- Assets/IslandEvents/Script/IIslandEvents.cs
+++ Assets/IslandEvents/Script/IIslandEvents.cs
@@ -2,6 +2,11 @@
 
 namespace PaddleHanks.IslandEvents.Script
 {
+    /// <summary>
+    /// Lewis Simmonds
+    /// 17/03/2021
+    /// abstract for all island events, which holds the text to show
+    /// </summary>
     public abstract class IslandsEvents : ScriptableObject
     {
         public string EventToOccur { get; set; }
